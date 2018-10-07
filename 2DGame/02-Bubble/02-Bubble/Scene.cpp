815cf6bf97ad8b6a2cdd2c1237e027011a3cf333
SkullKiddo <gabrielpuigdemunt@gmail.com>
2018-10-08 00:20:25 +0200

    Firts comit
    camera simple fixa al personatge

---
+++ 2DGame/02-Bubble/02-Bubble/Scene.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <cmath>
+#include <glm/gtc/matrix_transform.hpp>
+#include "Scene.h"
+#include "Game.h"
+
+
+#define SCREEN_X 0
+#define SCREEN_Y 128
+
+#define INIT_PLAYER_X_TILES 1
+#define INIT_PLAYER_Y_TILES 7
+
+
+Scene::Scene()
+{
+	map = NULL;
+	player = NULL;
+}
+
+Scene::~Scene()
+{
+	if(map != NULL)
+		delete map;
+	if(player != NULL)
+		delete player;
+}
+
+
+void Scene::init()
+{
+	initShaders();
+	map = TileMap::createTileMap("levels/cuevaLava.txt", glm::vec2(SCREEN_X, SCREEN_Y), texProgram); //"levels/cuevaLava.txt"     "levels/level01.txt" //TODO pasarli a create map una imatge de background
+	player = new Player();
+	player->init(glm::ivec2(SCREEN_X, SCREEN_Y), texProgram);
+	player->setPosition(glm::vec2(INIT_PLAYER_X_TILES * map->getTileSize(), INIT_PLAYER_Y_TILES * map->getTileSize()));
+	player->setTileMap(map);
+	projection = glm::ortho(0.f, float(SCREEN_WIDTH/2), float(SCREEN_HEIGHT - 1)/2, 0.f);
+	currentTime = 0.0f;
+}
+
+void Scene::update(int deltaTime)
+{
+	currentTime += deltaTime;
+	player->update(deltaTime);
+	float cameraOffsetX = -20 + player->posPlayer.x;
+	int maxOffsetX = float(SCREEN_WIDTH) + map->getMapSize().x+64; //TODO dependre el maxim offset de la camara del tamany de tile i personatge
+	cameraOffsetX = (cameraOffsetX < maxOffsetX)?cameraOffsetX : maxOffsetX; //el maxim
+	projection = glm::ortho(0.f + cameraOffsetX, float(SCREEN_WIDTH) + cameraOffsetX, float(SCREEN_HEIGHT - 1), 0.f);
+}
+
+void Scene::render()
+{
+	glm::mat4 modelview;
+
+	texProgram.use();
+	texProgram.setUniformMatrix4f("projection", projection);
+	texProgram.setUniform4f("color", 1.0f, 1.0f, 1.0f, 1.0f);
+	modelview = glm::mat4(1.0f);
+	texProgram.setUniformMatrix4f("modelview", modelview);
+	texProgram.setUniform2f("texCoordDispl", 0.f, 0.f);
+	map->render();
+	player->render();
+}
+
+void Scene::initShaders()
+{
+	Shader vShader, fShader;
+
+	vShader.initFromFile(VERTEX_SHADER, "shaders/texture.vert");
+	if(!vShader.isCompiled())
+	{
+		cout << "Vertex Shader Error" << endl;
+		cout << "" << vShader.log() << endl << endl;
+	}
+	fShader.initFromFile(FRAGMENT_SHADER, "shaders/texture.frag");
+	if(!fShader.isCompiled())
+	{
+		cout << "Fragment Shader Error" << endl;
+		cout << "" << fShader.log() << endl << endl;
+	}
+	texProgram.init();
+	texProgram.addShader(vShader);
+	texProgram.addShader(fShader);
+	texProgram.link();
+	if(!texProgram.isLinked())
+	{
+		cout << "Shader Linking Error" << endl;
+		cout << "" << texProgram.log() << endl << endl;
+	}
+	texProgram.bindFragmentOutput("outColor");
+	vShader.free();
+	fShader.free();
+}
+
+
+
---
+++ Mapas/Tiles/pont.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="pont" tilewidth="32" tileheight="32" tilecount="24" columns="3">
+ <image source="bridge.png" width="96" height="256"/>
+</tileset>
