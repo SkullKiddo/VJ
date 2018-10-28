5180953020be8f0ec148c6950c922fd7a020fa67
SkullKiddo <gabrielpuigdemunt@gmail.com>
2018-10-28 20:32:33 +0100

    Afegida clase basi Enemy y automatitzacio coses scene per enemics

--- 2DGame/02-Bubble/02-Bubble/HeavyBandit.h
+++ 2DGame/02-Bubble/02-Bubble/HeavyBandit.h
@@ -1,48 +1,15 @@
 #ifndef _HBANDIT_INCLUDE
 #define _HBANDIT_INCLUDE
 
+#include "Enemy.h"
 
-#include "Sprite.h"
-#include "TileMap.h"
-
-class HeavyBandit
+class HeavyBandit : public Enemy
 {
-
 public:
+
 	void init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram);
 	void update(int deltaTime);
-	void render();
 	void hit();
-
-	void setTileMap(TileMap *tileMap);
-	void setPosition();
-	glm::ivec2 pos;
-	box hitBox;
-	box calcHurtBox();
-	bool atacking;
-
-
-
-
-
-private:
-	Texture spritesheet;
-	Sprite *sprite;
-	bool alive;
-	int lifes;
-	bool vulnerable;
-	TileMap *map;
-	glm::ivec2 size;
-	glm::ivec2 colisionBox;
-	glm::ivec2 tileMapDispl;
-	glm::ivec2  colisionOffset;
-	bool dreta;
-
-	//atacar
-	bool chargingAtack;
-	float timeChargingAtack;
-	glm::ivec2 hitBoxOffset;
-
 };
 
 #endif
--- 2DGame/02-Bubble/02-Bubble/Skeleton.h
+++ 2DGame/02-Bubble/02-Bubble/Skeleton.h
@@ -1,54 +1,15 @@
 #ifndef _SKELETON_INCLUDE
 #define _SKELETON_INCLUDE
 
+#include "Enemy.h"
 
-#include "Sprite.h"
-#include "TileMap.h"
-
-class Skeleton
+class Skeleton : public Enemy
 {
 public:
-	//functions
+
 	void init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram);
 	void update(int deltaTime);
-	void render();
 	void hit();
-	void setTileMap(TileMap *tileMap);
-	void setPosition();
-	box calcHurtBox();
-
-	//attributes
-	bool atacking;
-	box hitBox;
-	glm::ivec2 pos;
-
-
-
-
-
-private:
-	//dibuixar
-	Texture spritesheet;
-	Sprite *sprite;
-
-	//dades
-	bool alive;
-	int lifes;
-	bool vulnerable;
-	bool dreta;
-
-	//atacar
-	bool chargingAtack;
-	float timeChargingAtack;
-	glm::ivec2 hitBoxOffset;
-
-	//tamanys
-	glm::ivec2 size;
-	glm::ivec2 colisionBox;
-	glm::ivec2 tileMapDispl;
-	glm::ivec2 colisionOffset;
-
-	TileMap *map;
 };
 
 #endif
